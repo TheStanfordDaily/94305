651b9de5af6aee461d831e07ab1f80cf727d4932
Sunnie <zixianma@DN0a22a06b.SUNet>
2019-05-06 16:29:26 -0700

    Updates

---
+++ precache-manifest.326a6a725363f69a4eab2e305019252f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "efe575ff53d0d0e86d5f5fc36f574e2b",
+    "url": "/94305/static/media/mobirise-icons.efe575ff.ttf"
+  },
+  {
+    "revision": "4efe53e2b45ddf36fb0ac902a39f2386",
+    "url": "/94305/static/media/ggra-975x604.4efe53e2.png"
+  },
+  {
+    "revision": "8a52668a5b69e8fabcb1",
+    "url": "/94305/static/js/runtime~main.30a77272.js"
+  },
+  {
+    "revision": "ad575204ef5658624b2b",
+    "url": "/94305/static/js/main.6b6886bc.chunk.js"
+  },
+  {
+    "revision": "a51f4b72259e674df491",
+    "url": "/94305/static/js/2.9c219576.chunk.js"
+  },
+  {
+    "revision": "ad575204ef5658624b2b",
+    "url": "/94305/static/css/main.80297a95.chunk.css"
+  },
+  {
+    "revision": "9a3bb24d00db34e4125cd5969791f11b",
+    "url": "/94305/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2694ff32ddd76e86c659f4e4d157d9c9.js"
+  "/94305/precache-manifest.326a6a725363f69a4eab2e305019252f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/94305/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
